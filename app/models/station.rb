23fa1cddf3de473e53dbb8391abed06c9a945f8e
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-03 11:42:50 -0700

    Add ability to create stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,5 @@
 class Station < ActiveRecord::Base
   validates :name, presence: true
-  validates :dock_count, presence: true
-  validates :city, presence: true
+  validates :dock_count, numericality: { only_integer: true }
   validates :installation_date, presence: true
-  
 end
