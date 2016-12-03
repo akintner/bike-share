0ba3ab68f8e6ef93186585e613fdf76c03e7aa67
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-03 15:55:47 -0700

    Fix typo

--- spec/features/user_can_delete_a_station_spec.rb
+++ spec/features/user_can_delete_a_station_spec.rb
@@ -7,7 +7,7 @@ describe "When a user wants to delete a station" do
     visit "/stations"
 
     within('#station_' + "#{station2.id}") do
-      click_on "delete"
+      click_on "Delete"
     end
     expect(page).not_to have_content(station2.name)
   end
@@ -16,7 +16,7 @@ describe "When a user wants to delete a station" do
     station1 = Station.create(name: "My House", dock_count: 3, installation_date: "March 3, 2000")
     visit "/stations/#{station1.id}"
 
-    click_on "delete"
+    click_on "Delete"
 
     expect(page).to have_current_path("/stations")
     expect(page).not_to have_content(station1.name)
