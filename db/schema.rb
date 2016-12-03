84d02fcc018689ea1732d40432ecc17d70f91d85
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-03 14:33:01 -0700

    Add cities table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203175319) do
+ActiveRecord::Schema.define(version: 20161203211814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stations", force: :cascade do |t|
     t.string   "name"
