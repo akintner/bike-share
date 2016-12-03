9ff22b9522311eb7f14abae67c8663da1791dae5
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-03 11:34:17 -0700

    Add stations to database

--- db/migrate/20161203175319_create_stations.rb
+++ db/migrate/20161203175319_create_stations.rb
@@ -3,8 +3,7 @@ class CreateStations < ActiveRecord::Migration[5.0]
     create_table :stations do |t|
       t.string :name
       t.integer :dock_count
-      t.string :city
-      t.string :installation_date
+      t.date :installation_date
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202055600) do
+ActiveRecord::Schema.define(version: 20161203175319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,7 @@ ActiveRecord::Schema.define(version: 20161202055600) do
   create_table "stations", force: :cascade do |t|
     t.string   "name"
     t.integer  "dock_count"
-    t.string   "city"
-    t.string   "installation_date"
+    t.date     "installation_date"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end
