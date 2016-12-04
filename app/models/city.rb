3034e727350877d6ee9355eedc0b6da7ea50f255
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-03 22:22:06 -0700

    Validate city names for uniqueness

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,4 @@
 class City < ActiveRecord::Base
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -7,5 +7,12 @@ describe "City" do
 
       expect(city).not_to be_valid
     end
+
+    it "validates uniqueness of name" do
+      city_1 = City.create(name: "Chicago")
+      city_2 = City.new(name: "Chicago")
+
+      expect(city_2).not_to be_valid
+    end
   end
 end
