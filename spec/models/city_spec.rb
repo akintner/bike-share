148df02c095481a696b06bec1b1da9edb62efbf0
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-04 10:56:10 -0700

    Failing spec for city has many stations

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -15,4 +15,22 @@ describe "City" do
       expect(city_2).not_to be_valid
     end
   end
+
+  context "relationships" do
+    let(:today)     { Date.today }
+    let(:yesterday) { Date.yesterday }
+
+    it "has many stations" do
+      station_1 = Station.create(name: "Downtown", dock_count: 12, installation_date: today)
+      station_2 = Station.create(name: "Uptown", dock_count: 22, installation_date: yesterday)
+      city = City.create(name: "Chicago")
+
+      city.stations << station_1
+      city.stations << station_2
+
+      expect(city.stations.count). to eq(2)
+      expect(city.stations.include?(station_1)).to be true
+      expect(city.stations.include?(station_2)).to be true
+    end
+  end
 end
