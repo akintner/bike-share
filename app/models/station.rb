6d6f575db559d90d041c6b3fc9a9f3df6ff1e557
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-04 14:02:22 -0700

    Add class methods to station and start station dashboard

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,4 +4,38 @@ class Station < ActiveRecord::Base
   validates :installation_date, presence: true
 
   belongs_to :city
+
+  def self.total
+    Station.count
+  end
+
+  def self.average_available_bikes
+    result = self.average("dock_count")
+    result.to_f
+  end
+
+  def self.most_bikes_available_at_station
+    self.maximum("dock_count")
+  end
+
+  def self.stations_with_most_bikes(number)
+    self.order("dock_count DESC").limit(number).pluck(:id)
+  end
+
+  def self.fewest_bikes_available_at_station
+    self.minimum("dock_count")
+  end
+
+  def self.stations_with_fewest_bikes(number)
+    self.order("dock_count ASC").limit(number).pluck(:id)
+  end
+
+  def self.most_recently_installed
+    self.order("installation_date DESC").limit(1).pluck(:id).first
+  end
+
+  def self.oldest_station
+    self.order("installation_date ASC").limit(1).pluck(:id).first
+  end
+
 end
