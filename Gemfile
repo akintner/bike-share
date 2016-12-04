e9200ea4f3fcb864ca89560e2174b1b236d3b160
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-03 23:02:57 -0700

    Add progress bar for importing cities

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra', require: 'sinatra/base'
 gem 'pg'
 gem 'activerecord'
 gem 'sinatra-activerecord'
+gem 'ruby-progressbar'
 
 group :development, :test do
   gem 'shotgun'
--- app/models/city_importer.rb
+++ app/models/city_importer.rb
@@ -2,16 +2,25 @@ require 'csv'
 
 class CityImporter
   attr_reader :filename,
-              :cities
+              :cities,
+              :number_of_lines
 
   def initialize(filename)
     @filename = filename
     @cities = []
+    @number_of_lines = `wc -l #{filename}`
   end
 
   def import
-    CSV.foreach(filename, headers: true) do |row|
-      City.create(name: row['city'])
+    bar = ProgressBar.create(title: "Cities", :total => number_of_lines.to_i)
+
+    City.transaction do
+      CSV.foreach(filename, headers: true) do |row|
+        City.create(name: row['city'])
+
+        bar.increment
+      end
+      bar.finish
     end
   end
 
