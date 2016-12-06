c76237436094079a6d50fb996e48a22d976b69f2
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-05 18:11:06 -0700

    Update how dates are formatted for station spec

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -4,14 +4,14 @@ describe "Station" do
   context "attributes are validated" do
     it "validates presence of name" do
         station = Station.create(dock_count: 30,
-                                installation_date: "4/13/2000"
+                                installation_date: Date.today
                                 )
         expect(station).not_to be_valid
     end
 
     it "validates presence of dock_count" do
       station = Station.create(name:"Market Street",
-                            installation_date: "4/13/2000"
+                            installation_date: Date.today
                             )
       expect(station).not_to be_valid
     end
@@ -19,7 +19,7 @@ describe "Station" do
     it "validates dock_count is an integer" do
       station = Station.new(name: "Market Street",
                               dock_count: 1.7,
-                              installation_date: "4/13/2000"
+                              installation_date: Date.today
                               )
       expect(station).not_to be_valid
     end
