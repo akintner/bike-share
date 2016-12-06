e917b566a24ccd9c16a0e7c36ab9efeaa0071603
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-06 00:05:50 -0700

    Enter weather conditions on a date

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -80,4 +80,18 @@ class BikeShareApp < Sinatra::Base
     @conditions = Condition.all.take(30)
     erb :"/conditions/index"
   end
+
+  get '/conditions/new' do
+    erb :"/conditions/new"
+  end
+
+  post '/conditions' do
+    condition = Condition.create(params[:condition])
+    redirect "/conditions/#{condition.id}"
+  end
+
+  get '/conditions/:id' do
+    @condition = Condition.find(params[:id])
+    erb :"/conditions/show"
+  end
 end
