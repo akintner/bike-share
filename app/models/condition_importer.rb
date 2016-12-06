2b4add30a06c6b2ea98349cfd2d1f8b289fff3e4
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-06 10:35:41 -0700

    Fix condition importer and tests

--- app/models/condition_importer.rb
+++ app/models/condition_importer.rb
@@ -4,32 +4,38 @@ class ConditionImporter
 
   def initialize(filename)
     @filename = filename
-    @number_of_lines = 'wc -l #{filename}'.to_i
+    @number_of_lines = `wc -l #{filename}`.to_i
   end
 
   def import
-    bar = ProgressBar.create(title: "Stations", total: number_of_lines)
+    bar = ProgressBar.create(title: "Conditions", total: number_of_lines)
 
     CSV.foreach(filename, headers: true) do |row|
-      date                    = to_date(row['date'])
-      max_temp_in_F           = row['max_temperature_f'].strip.to_f
-      mean_temp_in_F          = row['mean_temperature_f'].strip.to_f
-      min_temp_in_F           = row['min_temperature_f'].strip.to_f
-      humidity_percentage     = row['mean_humidity'].strip.to_f
-      visibility_in_miles     = row['mean_visibility_miles'].strip.to_f
-      mean_wind_speed_in_mph  = row['mean_wind_speed_mph'].strip.to_f
-      precipitation_in_inches = row['precipitation_inches'].strip.to_f
+      measurement_date     = to_date(row['date'])
+      max_temperature_f    = get_float(row['max_temperature_f'])
+      mean_temperature_f   = get_float(row['mean_temperature_f'])
+      min_temperature_f    = get_float(row['min_temperature_f'])
+      humidity_percent     = row['mean_humidity'].strip.to_f
+      visibility_miles     = row['mean_visibility_miles'].strip.to_f
+      mean_wind_speed_mph  = row['mean_wind_speed_mph'].strip.to_f
+      precipitation_inches = row['precipitation_inches'].strip.to_f
 
-      Condition.create(
-        date:                    date,
-        max_temp_in_F:           max_temp_in_F,
-        mean_temp_in_F:          mean_temp_in_F,
-        min_temp_in_F:           min_temp_in_F,
-        humidity_percentage:     humidity_percentage,
-        visibility_in_miles:     visibility_in_miles,
-        mean_wind_speed_in_mph:  mean_wind_speed_in_mph,
-        precipitation_in_inches: precipitation_in_inches
+
+
+      condition = Condition.new(
+        measurement_date:     measurement_date,
+        max_temperature_f:    max_temperature_f,
+        mean_temperature_f:   mean_temperature_f,
+        min_temperature_f:    min_temperature_f,
+        humidity_percent:     humidity_percent,
+        humidity_percent:     humidity_percent,
+        humidity_percent:     humidity_percent,
+        visibility_miles:     visibility_miles,
+        mean_wind_speed_mph:  mean_wind_speed_mph,
+        precipitation_inches: precipitation_inches
       )
+
+      condition.save if condition.valid?
 
       bar.increment
     end
@@ -40,5 +46,13 @@ class ConditionImporter
 
     def to_date(string)
       Date.strptime(string, '%m/%e/%Y')
+    end
+
+    def get_float(string)
+      if string == ''
+        nil
+      else
+        string.strip.to_f
+      end
     end
 end
