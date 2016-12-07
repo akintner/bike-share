3897ebd01be1784fbf51859ec3bf1bc2915cd0cc
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-12-07 14:31:04 -0700

    Update stations feature spec for relationships

--- spec/features/stations/user_sees_station_info_on_station_page_spec.rb
+++ spec/features/stations/user_sees_station_info_on_station_page_spec.rb
@@ -1,11 +1,14 @@
 require './spec/spec_helper'
 
 describe "When a user visits station page" do
+    let!(:city) { City.create(name: "Windy City") }
+
   it "they see station information" do
-    Station.create(name:"Market Street",
-                          dock_count: 30,
-                          installation_date: "March 3, 2000"
-                          )
+    station_1 = Station.create(name:"Market Street",
+                   dock_count: 30,
+                   installation_date: "March 3, 2000",
+                   city_id: city.id
+                  )
 
     station = Station.find_by(name: "Market Street")
     visit "/stations/#{station.id}"
