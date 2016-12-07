20984c483b0060e996c731cb93860c94b2c6dff3
Amz K <akintner@u.rochester.edu>
2016-12-06 22:24:48 -0700

    WIP update trip dashboard

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,13 +1,85 @@
+require 'pry'
+
 class Trip < ActiveRecord::Base
   validates :duration_in_seconds, numericality: { only_integer: true }
   validates :start_date,          presence: true
   validates :end_date,            presence: true
   validates :start_station,       presence: true
   validates :end_station,         presence: true
+  validates :bike_id,             presence: true
   validates :subscription_type,   presence: true
   validates :zipcode_id,          presence: true
 
   belongs_to :start_station, :class_name => "Station", :foreign_key => "start_station_id"
   belongs_to :end_station,   :class_name => "Station", :foreign_key => "end_station_id"
+
+  def self.total
+    Trip.count
+  end
+
+  def self.average_duration_of_ride
+    self.average("duration_in_seconds").to_f.round(0)
+  end
+
+  def self.longest_ride
+    self.maximum("duration_in_seconds")
+  end
+
+  def self.shortest_ride
+    self.minimum("duration_in_seconds")
+  end
+
+  def self.most_rides_starting
+    most = self.select("start_station_id").group("start_station_id").count
+    most.values.max
+  end
+
+  def self.most_rides_ending
+    most = self.select("end_station_id").group("end_station_id").count
+    most.values.max
+  end
+
+  def self.rides_per_month(month)
+    
+    self.where(end_date: month).count
+  end
+
+  def self.rides_per_year(year)
+    self.select("end_date").group("end_date")
+  end
+
+  def self.most_ridden_bike
+    most = self.select("bike_id").group("bike_id").count
+    
+    puts most.values.max
+  end
+  
+  def self.least_ridden_bike
+    least = self.select("bike_id").group("bike_id").count
+    
+    puts least.values.min
+  end
+
+  def self.number_customers
+    self.where(subscription_type: "Customer").count
+  end
+
+  def self.number_subscribers
+    self.where(subscription_type: "Subscriber").count
+  end
+
+  def self.user_percentage(user_type)
+    self.where(subscription_type: user_type).count / Trip.count
+  end
+
+  def self.highest_trip_date
+    most = self.select("end_date").group("end_date").count
+    most.values.max
+  end
+
+  def self.lowest_trip_date
+    least = self.select("end_date").group("end_date").count
+    least.values.min
+  end
 
 end
---
+++ spec/models/trips_dashboard_spec.rb
@@ -0,0 +1,134 @@
+require './spec/spec_helper'
+require 'pry'
+ 
+ describe "Trip" do
+   context "analytics" do
+     let!(:start_station) { Station.create(name: "Starter", dock_count: 11, installation_date: Date.today) }
+     let!(:end_station) { Station.create(name: "Finisher", dock_count: 11, installation_date: Date.today) }
+ 
+     it "can find total count of trips" do
+       trip1 = Trip.new(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       trip2 = Trip.new(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", bike_id: 17, subscription_type: "Subscriber", zipcode_id: 81420)
+       trip1.start_station = start_station
+       trip1.end_station = end_station
+       trip2.start_station = start_station
+       trip2.end_station = end_station
+       trip1.save
+       trip2.save
+       binding.pry
+       expect(Trip.total).to eq(2)
+     end
+ 
+     it "can find average duration of a ride" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.average_duration_of_ride).to eq(176)
+     end
+ 
+     it "can find the longest ride" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.longest_ride).to eq(271)
+     end
+ 
+     it "can find the shortest ride" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.shortest_ride).to eq(69)
+     end
+ 
+     it "can find station with most rides starting there" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.most_rides_starting).to eq(7)
+     end
+ 
+     it "can find station with most rides ending there" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.most_rides_ending).to eq(12)
+     end
+ 
+     it "can find trip rides per month" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       
+       expect(Trip.rides_per_month("September")).to eq(4)
+    end
+ 
+    it "can find trip rides per year" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       
+       expect(Trip.rides_per_year("1989")).to eq(4)
+     end
+ 
+    it "can find most ridden bike" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 7, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+       
+       expect(Trip.most_ridden_bike).to eq(17)
+    end
+ 
+     it "can find least ridden bike" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 7, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 562, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.least_ridden_bike).to eq(7)
+     end
+
+     it "can find user subscription stats" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 2, bike_id: 7, subscription_type: "Subscriber", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 9, subscription_type: "Subscriber", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 367, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 9, subscription_type: "Subscriber", zipcode_id: 81420)
+ 
+       expect(Trip.number_customers).to eq(2)
+       expect(Trip.number_subscribers).to eq(3)
+       expect(Trip.user_percentage("Customer")).to eq(40)
+       expect(Trip.user_percentage("Suscriber")).to eq(60)
+     end
+     
+     it "can find highest trip date" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 15, 1989", end_date: "September 15, 1989", start_station_id: 1, end_station_id: 1, bike_id: 7, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 29, 1989", end_date: "September 29, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 2, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+       
+       expect(Trip.highest_trip_date).to eq("1989-09-09")
+     end
+ 
+     it "can find lowest trip date" do
+       Trip.create(duration_in_seconds: 187, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 69, start_date: "September 19, 1989", end_date: "September 19, 1989", start_station_id: 1, end_station_id: 2, bike_id: 7, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 271, start_date: "September 29, 1989", end_date: "September 29, 1989", start_station_id: 1, end_station_id: 1, bike_id: 17, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 560, start_date: "September 9, 1989", end_date: "September 9, 1989", start_station_id: 2, end_station_id: 1, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 363, start_date: "September 19, 1989", end_date: "September 19, 1989", start_station_id: 2, end_station_id: 2, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+       Trip.create(duration_in_seconds: 561, start_date: "September 19, 1989", end_date: "September 19, 1989", start_station_id: 2, end_station_id: 1, bike_id: 9, subscription_type: "Customer", zipcode_id: 81420)
+ 
+       expect(Trip.lowest_trip_date).to eq("1989-09-19")
+     end
+   end
+ end 
